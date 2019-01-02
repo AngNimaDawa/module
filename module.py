b984dc37533ff5cbaffa6688b5b0a363532693ee
AngNimaDawa <45613061+AngNimaDawa@users.noreply.github.com>
2019-01-02 08:57:58 +0545

    Create module.py

---
+++ module.py
@@ -0,0 +1,16 @@
+# #!/usr/bin/env python3
+
+# """
+# This is chess module
+# """
+# print("i'm chess module")
+# print(__name__)
+# def main():
+# 	"""
+# 	This is main function do nothing
+# 	"""
+# 	print("i'm main function in mno module")
+
+# if __name__=="__main__":
+# 	main()
+
